aa500c5f893bc1aa77c3aac0dac5c64828bf9491
zhl <zhl@qq.com>
2020-02-11 15:14:34 +0800

    add display weatherInfomation function

---
+++ app/src/main/java/com/test/coolweather/gson/AQI.java
@@ -0,0 +1,12 @@
+package com.test.coolweather.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+
+}
